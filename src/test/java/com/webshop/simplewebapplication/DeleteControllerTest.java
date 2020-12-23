1b4ac68161b6fe00270ef98cec3db5223680659a
Antonina Bahareva <astasenash@gmail.com>
2020-12-23 11:40:45 +0500

    оставшиеся тесты

--- src/test/java/com/webshop/simplewebapplication/DeleteControllerTest.java
+++ src/test/java/com/webshop/simplewebapplication/DeleteControllerTest.java
@@ -1,6 +1,7 @@
 package com.webshop.simplewebapplication;
 
 import com.webshop.simplewebapplication.controller.RegistrationController;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,6 +9,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,4 +26,11 @@ public class DeleteControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Test
+    public void deleteItemTest() throws Exception {
+        this.mockMvc.perform(post("/account/delete"))
+                .andDo(print())
+                .andExpect(xpath("//*[@id='err409']").string("HTTP 409 Conflict. You need to delete all your items to delete account."));
+    }
 }
